fdd9fc607af4c614278cb8853a362c2e2a4d70e0
youngjin.kim2 <youngjin.kim2@navercorp.com>
2022-08-23 23:54:44 +0900

    [#18] Applied snowflake uuid

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { authProviders } from './auth/auth.providers';
 import { databaseProviders } from './database.providers';
+import { UuidService } from './snowflake.service';
 
 @Module({
-    providers: [...databaseProviders, ...authProviders],
-    exports: [...databaseProviders, ...authProviders],
+    providers: [...databaseProviders, ...authProviders, UuidService],
+    exports: [...databaseProviders, ...authProviders, UuidService],
 })
 export class DatabaseModule {}
---
+++ server/src/database/snowflake.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Worker } from 'snowflake-uuid';
+
+@Injectable()
+export class UuidService {
+    worker: Worker;
+
+    constructor() {
+        this.worker = new Worker(0, 0, {
+            workerIdBits: 5,
+            datacenterIdBits: 5,
+            sequenceBits: 12,
+        });
+    }
+
+    id(): number {
+        return Number(this.worker.nextId());
+    }
+}
